c934a1d42fbd9c95b4d53102ee9bc7bd64b7e0aa
lizz <363664470@qq.com>
2018-09-17 17:55:15 +0800

    tiny

--- deb_deps.sh
+++ deb_deps.sh
@@ -6,10 +6,10 @@ sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev 
 sudo apt-get install --no-install-recommends libboost-all-dev -y
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev -y
 sudo apt-get install libatlas-base-dev -y
-sudo apt-get install gcc-4.9-multilib g++-4.9-multilib -y
+# sudo apt-get install gcc-4.9-multilib g++-4.9-multilib -y
 sudo apt-get install libf2c2-dev -y
 sudo apt-get install libglew-dev freeglut3 freeglut3-dev -y
 sudo apt-get install premake4 -y
+sudo apt-get install swig3.0 python3-dev python3-pip -y
 ## This is not necessary
 #  sudo apt-get install cuda
-#  sudo apt-get install swig3.0 python3-dev python3-pip -y
