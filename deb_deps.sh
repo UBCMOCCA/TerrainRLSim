670edf644b69c24c10ae7467835014eb72a4d603
Glen <Neo-X@users.noreply.github.com>
2020-01-17 11:18:09 -0800

    remove sudo

--- deb_deps.sh
+++ deb_deps.sh
@@ -2,14 +2,14 @@
 #
 # Install the system dependencies required to build TerrainRL
 #
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler -y
-sudo apt-get install --no-install-recommends libboost-all-dev -y
-sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev -y
-sudo apt-get install libatlas-base-dev -y
-sudo apt-get install gcc-4.9-multilib g++-4.9-multilib -y
-sudo apt-get install libf2c2-dev -y
-sudo apt-get install libglew-dev freeglut3 freeglut3-dev -y
-sudo apt-get install premake4 -y
+apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler -y
+apt-get install --no-install-recommends libboost-all-dev -y
+apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev -y
+apt-get install libatlas-base-dev -y
+apt-get install gcc-4.9-multilib g++-4.9-multilib -y
+apt-get install libf2c2-dev -y
+apt-get install libglew-dev freeglut3 freeglut3-dev -y
+apt-get install premake4 -y
 ## This is not necessary
 #  sudo apt-get install cuda
 #  sudo apt-get install swig3.0 python3-dev python3-pip -y
