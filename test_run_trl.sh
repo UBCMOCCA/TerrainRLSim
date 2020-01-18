e9bdb2cac91c14e137e3a02278866855ab7dc2af
Neo-X <glen@fracturedplane.com>
2020-01-18 11:54:35 -0800

    Working on docker file

---
+++ test_run_trl.sh
@@ -0,0 +1,3 @@
+docker build -f Dockerfile_trl -t rlframe_trl:latest .
+
+docker run --rm -it rlframe_trl:latest /bin/bash -c "pushd /root/playground/TerrainRLSim; git pull origin master; python3 simAdapter/terrainRLSimTest.py"
